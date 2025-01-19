46f47f6cb1bcedc98232a85c9429b2a39bf0154a
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-01-19 02:46:27 +0000

    ollama works

---
+++ main.py
@@ -0,0 +1,9 @@
+from ollama import chat
+from ollama import ChatResponse
+
+response: ChatResponse = chat(
+    model="llama3.2", messages=[
+        {"role": "user", "content": "Why is the sky blue?"}
+    ]
+)
+print(response.message.content)
